09fdcf53978f1992f51f9c6ce2639f402b392bac
Kevin Sung <kevin.y.sung@gmail.com>
2021-07-02 14:02:48 -0700

    work on script only need to create json formatted file

--- generate_json_reading_plan_from_spreadsheet.py
+++ generate_json_reading_plan_from_spreadsheet.py
@@ -7,11 +7,14 @@ and Verses (i.e. John 1) and call the LSM API to generate a
 json file of the verses requested.
 """
 
+json_string = ""
+
 with open('reading_plan/Sample_Bible_Reading_Program.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
     	if not (row[0]=="Date" or row[1] =="Verses"):
-    		print(requests.get("https://api.lsm.org/recver.php?String=" + row[1] + "&Out=json").text)
+    		json_string += requests.get("https://api.lsm.org/recver.php?String=" + row[1] + "&Out=json").text
     		line_count += 1
+    print(json_string)
     print(f'Processed {line_count} lines.')
